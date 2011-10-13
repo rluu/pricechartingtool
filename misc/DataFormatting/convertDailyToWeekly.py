326eec10d9a73842466ae9e837fa4ae9467d6108
rluu <rluu@8d161d6a-30f3-4866-9114-8b15788782b4>
2011-10-13 23:18:07 +0000

    Finished writing and testing convertDailyToWeekly.py.

--- misc/DataFormatting/convertDailyToWeekly.py
+++ misc/DataFormatting/convertDailyToWeekly.py
@@ -21,6 +21,8 @@ import copy
 # For parsing command-line options
 from optparse import OptionParser  
 
+import datetime
+
 ##############################################################################
 # Global Variables
 ##############################################################################
@@ -64,7 +66,7 @@ parser.add_option("--input-file",
                   type="str",
                   dest="inputFile",
                   default=None,
-                  help="Specify input pricebar CSV data file.  This file should have daily pricebars.",
+                  help="Specify input pricebar CSV data file.  This file should have daily pricebars, and it is assumed that the file has pricebars ordered from oldest to newest.",
                   metavar="<FILE>")
 
 parser.add_option("--output-file",
@@ -110,18 +112,26 @@ else:
         
 ##############################################################################
 
-print("ERROR:  Program is not completely coded.  Please finish coding and testing this script to do what we want it to do.")
-sys.exit(1)
-
-
 # Lines in the destination file.
 convertedLines = []
+
+currentWeekHasValues = False
+currentWeekDateStr = ""
+currentWeekOpen = -1
+currentWeekHigh = -1
+currentWeekLow = -1
+currentWeekClose = -1
+currentWeekVolume = -1
+currentWeekOpenInt = -1
+
+currentWeekIsoYearNumber = -1
+currentWeekIsoWeekNumber = -1
 
 # Read input file.
 with open(inputFile) as f:
     i = 0
     for line in f:
-        if i >= linesToSkip:
+        if i >= linesToSkip and line.strip() != "":
 
             # Check the number of fields.
             fields = line.split(",")
@@ -149,11 +159,140 @@ with open(inputFile) as f:
             monthStr = dateStr[0:2]
             dayStr = dateStr[3:5]
             yearStr = dateStr[6:10]
-            
-            convertedLines.append(convertedLine)
-            
-        i += 1
 
+            month = int(monthStr)
+            day = int(dayStr)
+            year = int(yearStr)
+
+            d = datetime.date(year, month, day)
+
+            isoWeekNumber = -1
+            isoYearNumber = -1
+            isoWeekdayNumber = -1
+            (isoYearNumber, isoWeekNumber, isoWeekdayNumber) = d.isocalendar()
+            
+            #print("DEBUG: " + \
+            #      "isoYearNumber={}, isoWeekNumber={}, isoWeekdayNumber={}".\
+            #      format(isoYearNumber, isoWeekNumber, isoWeekdayNumber))
+            
+            if currentWeekHasValues == False:
+                #print("DEBUG: currentWeekHasValues == {}".\
+                #      format(currentWeekHasValues))
+                
+                currentWeekDateStr = dateStr
+                currentWeekOpen = float(openStr)
+                currentWeekHigh = float(highStr)
+                currentWeekLow = float(lowStr)
+                currentWeekClose = float(closeStr)
+                currentWeekVolume = int(volumeStr)
+                currentWeekOpenInt = int(openIntStr)
+
+                currentWeekIsoYearNumber = isoYearNumber
+                currentWeekIsoWeekNumber = isoWeekNumber
+                currentWeekHasValues = True
+                continue
+            else:
+                
+                #print("DEBUG: isoYearNumber={}, currentWeekIsoYearNumber={}".\
+                #      format(isoYearNumber, currentWeekIsoYearNumber))
+                #print("DEBUG: isoWeekNumber={}, currentWeekIsoWeekNumber={}".\
+                #      format(isoWeekNumber, currentWeekIsoWeekNumber))
+
+                if isoYearNumber < currentWeekIsoYearNumber:
+                    print("Error: The input file is supposed to be " + \
+                          "in chronological order!  Failed at line: {}".\
+                          format(line))
+                    sys.exit(1)
+                    
+                elif isoYearNumber == currentWeekIsoYearNumber and \
+                         isoWeekNumber < currentWeekIsoWeekNumber:
+                    print("Error: The input file is supposed to be " + \
+                          "in chronological order!  Failed at line: {}".\
+                          format(line))
+
+                    sys.exit(1)
+                    
+                elif isoYearNumber == currentWeekIsoYearNumber and \
+                       isoWeekNumber == currentWeekIsoWeekNumber:
+                    # The new date is in the same week as 'currentWeek'.
+                    # Update the values as required.
+                    print("DEBUG: Date {} is within SAME week.".\
+                          format(d.isoformat()))
+                    
+                    priceValues = []
+                    priceValues.append(float(openStr))
+                    priceValues.append(float(highStr))
+                    priceValues.append(float(lowStr))
+                    priceValues.append(float(closeStr))
+                    priceValues.sort()
+
+                    smallestValue = priceValues[0]
+                    largestValue = priceValues[-1]
+
+                    if smallestValue < currentWeekLow:
+                        currentWeekLow = smallestValue
+                    if largestValue > currentWeekHigh:
+                        currentWeekHigh = largestValue
+                    currentWeekClose = float(closeStr)
+                    currentWeekVolume += int(volumeStr)
+                    currentWeekOpenInt = int(openIntStr)
+                    
+                elif (isoYearNumber == currentWeekIsoYearNumber and \
+                      isoWeekNumber > currentWeekIsoWeekNumber) or \
+                      (isoYearNumber > currentWeekIsoYearNumber):
+                    
+                    print("DEBUG: Date {} is in a NEW week.".\
+                          format(d.isoformat()))
+
+                    # The new date is in a week that is later than the
+                    # what the 'currentWeek' is.  This means the week
+                    # has completed and we need to create and add a
+                    # line of text for that week.
+                    convertedLine = \
+                        "{},{},{},{},{},{},{}".format(currentWeekDateStr,
+                                                      currentWeekOpen,
+                                                      currentWeekHigh,
+                                                      currentWeekLow,
+                                                      currentWeekClose,
+                                                      currentWeekVolume,
+                                                      currentWeekOpenInt)
+                    convertedLines.append(convertedLine)
+
+                    # Set new values for the 'currentWeek'.
+                    currentWeekDateStr = dateStr
+                    currentWeekOpen = float(openStr)
+                    currentWeekHigh = float(highStr)
+                    currentWeekLow = float(lowStr)
+                    currentWeekClose = float(closeStr)
+                    currentWeekVolume = int(volumeStr)
+                    currentWeekOpenInt = int(openIntStr)
+
+                    currentWeekIsoYearNumber = isoYearNumber
+                    currentWeekIsoWeekNumber = isoWeekNumber
+                    currentWeekHasValues = True
+                    
+                else:
+                    print("DEBUG: Should never get here.")
+                    
+                    
+        i += 1
+        
+    # Now done with going through all the lines.  Close out the last
+    # week as a line.
+    if currentWeekHasValues == True:
+        convertedLine = \
+            "{},{},{},{},{},{},{}".format(currentWeekDateStr,
+                                          currentWeekOpen,
+                                          currentWeekHigh,
+                                          currentWeekLow,
+                                          currentWeekClose,
+                                          currentWeekVolume,
+                                          currentWeekOpenInt)
+        convertedLines.append(convertedLine)
+
+
+print("DEBUG: Total number of weeks in output file is: {}".\
+      format(len(convertedLines)))
 
 # Insert header line.
 convertedLines.insert(0, headerLine)
@@ -161,7 +300,7 @@ convertedLines.insert(0, headerLine)
 # Write to file, truncating if it already exists.
 with open(outputFile, "w") as f:
     for line in convertedLines:
-        f.write(line.rstrip() + newline)
+        f.write(line + newline)
         
 print("Done.")
 
